2403fc6c0ed084ebbabf5e68ffae3d5077f85594
narutiga <100295602+narutiga@users.noreply.github.com>
2022-11-19 20:21:54 +0900

    🚧 index.tsx作業中

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import { Comment } from "src/component/Comment";
+import { Form } from "src/component/Form";
+
 const Home = () => {
-  return <div>テスト</div>;
+  return (
+    <div>
+      <Form />
+      <Comment gestName="kino" postedAt="2022-11-19" title="テストですよー！" />
+    </div>
+  );
 };
 
 export default Home;
