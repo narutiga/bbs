dca7c1cb77a08bfe0fb24aed20d1db36944d1cf9
narutiga <100295602+narutiga@users.noreply.github.com>
2022-11-20 10:04:14 +0900

    ✨ axios導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,50 @@
-import { Comment } from "src/component/Comment";
-import { Form } from "src/component/Form";
+import axios from "axios";
+import requests from "src/lib/Requests";
+import { CommentCard } from "src/component/CommentCard";
+import { CommentForm } from "src/component/CommentForm";
+import { useEffect, useState } from "react";
+import { CommentProps } from "src/component/CommentCard/CommentCard";
+import { PaginationComponent } from "src/component/PaginationComponent";
+
+type CommentData = {
+  id: string;
+  title: string;
+  guestName: string;
+  posted_at: string;
+};
 
 const Home = () => {
+  const [comments, setComments] = useState<CommentData[]>([]);
+
+  useEffect(() => {
+    const getCommentData = () => {
+      axios
+        .get(requests.fetchCommentData)
+        .then((res) => {
+          setComments(res.data);
+          console.log(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getCommentData();
+  }, []);
+
   return (
-    <div>
-      <Form />
-      <Comment gestName="kino" postedAt="2022-11-19" title="テストですよー！" />
+    <div className="max-w-lg mr-auto ml-auto">
+      <CommentForm />
+      {comments.map((comment) => {
+        return (
+          <CommentCard
+            key={comment.id}
+            guestName={comment.guestName}
+            postedAt={comment.posted_at}
+            title={comment.title}
+          />
+        );
+      })}
+      <PaginationComponent />
     </div>
   );
 };
