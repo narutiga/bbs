e53c573e795f947e17d21c23809821a25260ddb7
narutiga <100295602+narutiga@users.noreply.github.com>
2022-11-20 10:03:25 +0900

    ♻️ component名を修正

--- src/component/CommentForm/CommentForm.tsx
+++ src/component/CommentForm/CommentForm.tsx
@@ -1,35 +1,40 @@
 import { Box, Button, Group, Textarea, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { ComponentProps, FC } from "react";
+import axios from "axios";
+import requests from "src/lib/Requests";
 
-type Chat = {
+export type Comment = {
   guestName: string;
-  comment: string;
+  title: string;
 };
 
-export const Form = () => {
-  const form = useForm<Chat>({
+export const CommentForm = () => {
+  const form = useForm<Comment>({
     initialValues: {
       guestName: "",
-      comment: "",
+      title: "",
     },
 
     validate: {
       guestName: (value) =>
         value.length < 2 ? "名前は２文字以上で入力してください" : null,
-      comment: (value) =>
+      title: (value) =>
         value.length < 1 ? "コメントを入力してください" : null,
     },
   });
 
-  const handleSubmit = (value: Chat) => {
+  const handleSubmitComment = (value: Comment) => {
+    axios.post(requests.InsertCommentData, {
+      guestName: value.guestName,
+      title: value.title,
+    });
     console.log(value);
     form.reset();
   };
 
   return (
     <Box sx={{ maxWidth: 300, margin: 100 }} mx="auto">
-      <form onSubmit={form.onSubmit(handleSubmit)}>
+      <form onSubmit={form.onSubmit(handleSubmitComment)}>
         <TextInput
           required
           label="お名前"
@@ -40,10 +45,12 @@ export const Form = () => {
           required
           label="コメント"
           placeholder="comment"
-          {...form.getInputProps("comment")}
+          {...form.getInputProps("title")}
         />
         <Group position="right" mt="md">
-          <Button type="submit">投稿</Button>
+          <Button type="submit" color="indigo">
+            投稿
+          </Button>
         </Group>
       </form>
     </Box>
