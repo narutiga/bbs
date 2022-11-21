e2e80e9fdabbe3aa73501d78f981752e4306b8ec
narutiga <100295602+narutiga@users.noreply.github.com>
2022-11-21 19:35:44 +0900

    🎨 Layoutを作成

--- src/pages_layout/Layout/Layout.tsx
+++ src/pages_layout/Layout/Layout.tsx
@@ -1,10 +1,15 @@
 import { CustomLayout } from "next";
+import { Footer } from "src/component/Footer";
+import { HeaderMenu } from "src/component/HeaderMenu";
+import { PaginationComponent } from "src/component/PaginationComponent";
 
 /** @package */
 export const Layout: CustomLayout = (page: any) => {
   return (
-    <div>
-      <main>{page}</main>
+    <div className="flex min-h-screen flex-col">
+      <HeaderMenu />
+      <main className="flex flex-1 flex-col pt-10 items-center">{page}</main>
+      <Footer />
     </div>
   );
 };
