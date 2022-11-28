fcb295592455d26940c265548b6b12ada77252da
narutiga <100295602+narutiga@users.noreply.github.com>
2022-11-28 20:33:08 +0900

    🚧 トップへ戻るボタン調整中

--- src/component/ScrollToTop/ScrollToTop.tsx
+++ src/component/ScrollToTop/ScrollToTop.tsx
@@ -1,5 +1,5 @@
 import { useWindowScroll } from "@mantine/hooks";
-import { Button, Text, Group } from "@mantine/core";
+import { Button, Text, Group, Affix, Transition } from "@mantine/core";
 import { FC } from "react";
 import { IconChevronsUp } from "@tabler/icons";
 
@@ -8,16 +8,21 @@ export const ScrollToTop: FC = () => {
   const [scroll, scrollTo] = useWindowScroll();
 
   return (
-    <Group position="right">
-      <Button
-        onClick={() => scrollTo({ y: 0 })}
-        variant="gradient"
-        gradient={{ from: "indigo.3", to: "cyan.3" }}
-        className="px-0 h-12 w-12"
-        radius="xl"
-      >
-        <IconChevronsUp />
-      </Button>
-    </Group>
+    <Affix position={{ bottom: 20, right: 20 }}>
+      <Transition transition="slide-up" mounted={scroll.y > 0}>
+        {(transitionStyles) => (
+          <Button
+            onClick={() => scrollTo({ y: 0 })}
+            variant="gradient"
+            gradient={{ from: "indigo.3", to: "cyan.3" }}
+            className="px-0 h-12 w-12"
+            radius="xl"
+            style={transitionStyles}
+          >
+            <IconChevronsUp />
+          </Button>
+        )}
+      </Transition>
+    </Affix>
   );
 };
